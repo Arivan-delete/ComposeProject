e21839afa98dac69fa06f050b4d7e14c2ac8bbbf
Artem <artem.mamedov5375@gmail.com>
2024-02-07 18:59:27 +0200

    SwipeToDismiss

--- app/src/main/java/com/example/composeproject/MainActivity.kt
+++ app/src/main/java/com/example/composeproject/MainActivity.kt
@@ -6,15 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismiss
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.example.composeproject.ui.theme.ComposeProjectTheme
 import com.example.composeproject.ui.theme.ProfileCard
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +40,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Test(viewModel: MainViewModel) {
     ComposeProjectTheme {
@@ -36,11 +51,36 @@ fun Test(viewModel: MainViewModel) {
         ) {
             val models = viewModel.models.observeAsState(listOf())
             LazyColumn {
-                items(models.value) { model ->
-                    ProfileCard(
-                        model = model,
-                        onFollowedButtonClickListener = {
-                            viewModel.changeFollowingStatus(it)
+                items(models.value, key = { it.id }) { model ->
+                    val dismissState = rememberDismissState()
+                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                        viewModel.delete(model)
+                    }
+                    SwipeToDismiss(
+                        state = dismissState,
+                        directions = setOf(DismissDirection.EndToStart),
+                        background = {
+                            Box(
+                                contentAlignment = Alignment.CenterEnd,
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .fillMaxSize()
+                                    .background(Color.Red.copy(alpha = 0.5f))
+                            ) {
+                                Text(
+                                    text = "Delete Item",
+                                    color = Color.White,
+                                    fontSize = 24.sp
+                                )
+                            }
+                        },
+                        dismissContent = {
+                            ProfileCard(
+                                model = model,
+                                onFollowedButtonClickListener = {
+                                    viewModel.changeFollowingStatus(it)
+                                }
+                            )
                         }
                     )
                 }
